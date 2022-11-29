f3da14d6d86321b578b80dd458f26542086c75e9
kayode <officialkayodeolowo@gmail.com>
2022-11-29 11:59:41 -0800

    axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
 import {ThemeProvider} from './context/ThemeContext'
+import {Home, Account, Signin, Signup } from './Pages';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [coins, setCoins] = useState([])
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true'
+
+  useEffect (()=> {
+    axios.get(url).then((response)=> {
+      setCoins(response.data)
+      console.log(response.data)
+    })
+  }, [url])
+
   return (
    <ThemeProvider> 
         <Navbar/>
+        <Routes> 
+          <Route path='/' element={<Home coins={coins} />} />
+           <Route path='/signin' element={<Signin/>} />
+            <Route path='/signup' element={<Signup/>} />
+             <Route path='/account' element={<Account/>} />
+        </Routes>
    </ThemeProvider>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import SearchCoin from '../components/SearchCoin'
 
-const Home = () => {
+const Home = ({coins}) => {
   return (
-    <div>Home</div>
+    <div>
+      <SearchCoin coins={coins}/>
+    </div>
   )
 }
 
